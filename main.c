f82b28821d43934c1d7c07a5f36f7ae3dc592952
Sébastien Carluer <sebastien@fuzzinglabs.com>
2022-10-26 15:34:25 +0200

    test

--- main.c
+++ main.c
@@ -16,3 +16,24 @@ int main() {
     lecture_fichier();
     return 0;
 }
+
+// Récupération du fichier donné par l'utilisateur
+
+// Extraction des octets du fichier
+
+// Création d'une liste avec tous les caractères différents
+
+// Pour chacun de ces charactères
+    // Compter le nombre d'occurence
+
+// Associer chaque charactère avec son nombre d'occurences
+
+// Création d'un arbre binaire pour chaque duo Valeur / Fréquence
+
+// Tant qu'il ni a pas de noeud final
+    // On fusionne les arbres binaire avec les plus petites fréquences
+
+// On créer un code de Huffman pour réécrire le fichier de façon compréssé
+
+// On retourne le code de Huffman
+// On retourne le fichier compressé
