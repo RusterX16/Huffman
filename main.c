3900901b17f71feb284dd3d7654afd30ba3cc742
Sébastien Carluer <sebastien@fuzzinglabs.com>
2022-10-26 15:22:20 +0200

    liste des actions à faire

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    // Récupération du fichier donné par l'utilisateur
+
+    // Extraction des octets du fichier
+
+    // Création d'une liste avec tous les caractères différents
+
+    // Pour chacun de ces charactères
+        // Compter le nombre d'occurence
+
+    // Associer chaque charactère avec son nombre d'occurences
+
+    // Création d'un arbre binaire pour chaque duo Valeur / Fréquence
+
+    // Tant qu'il ni a pas de noeud final
+        // On fusionne les arbres binaire avec les plus petites fréquences
+
+    // On créer un code de Huffman pour réécrire le fichier de façon compréssé
+
+    // On retourne le code de Huffman
+    // On retourne le fichier compressé
+
     return 0;
 }
